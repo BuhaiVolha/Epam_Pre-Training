ba03e939f70a0f91889589d14e95fec335cce850
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-01-30 14:15:59 +0300

    Update Printer.java

--- Task2/Printer.java
+++ Task2/Printer.java
@@ -7,11 +7,18 @@ public class Printer {
     public static void print(String msg) {
         System.out.println(msg);
     }
+    
     public static void printThere(String msg) {
         System.out.print(msg);
     }
+    
     public static int inputInt(String msg){
         print(msg);
         return new Scanner(System.in).nextInt();
+    }
+    
+    public static String inputLine(String msg){
+        print(msg);
+        return new Scanner(System.in).nextLine();
     }
 }
