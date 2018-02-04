3bcdd9ff3a4bb89124fc312e3a444a39abd81ac4
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-04 11:25:52 +0300

    Update NextDayDater.java

--- Task2/4/NextDayDater.java
+++ Task2/4/NextDayDater.java
@@ -6,50 +6,51 @@ public class NextDayDater {
     public static void main(String[] args) {
         int day, month, year;
 
-        print("Это приложение рассчитывает дату следующего дня.");
-        print("!!!Рекомендуемый минимальный год - 1582, иначе\nформула рассчета високосных годов не сработает.\n");
+        print("This application calculates the date of the next day.");
+        print("!!!The recommended minimum for a year is 1582, otherwise\n"
+              + "leap year calculation formula might not work.\n");
 
-        day = inputInt("Введите число месяца:");
+        day = inputInt("Input the day:");
         while (!NextDayDaterLogic.validateDay(day)) {
-            print("Минимальное число - 01, максимальное - 31!\n");
-            day = inputInt("Введите число месяца:");
+            print("The minimum input is 01, the maximum one 31!\n");
+            day = inputInt("Input the day:");
         }
 
-        month = inputInt("Введите порядковый номер месяца:");
+        month = inputInt("Input the month:");
         while (!NextDayDaterLogic.validateMonth(month)) {
-            print("Минимальное число - 01, максимальное - 12!\n");
-            month = inputInt("Введите порядковый номер месяца:");
+            print("The minimum input is 01, the maximum one 12!\n");
+            month = inputInt("Input the month:");
         }
         if (NextDayDaterLogic.isMonthWith30Days(month)) {
             while (day > 30) {
-                System.out.printf("В %s-ом месяце всего 30 дней!\n", month);
-                day = inputInt("Введите число месяца:");
+                System.out.printf("This month (%s) has only 30 days!\n", month);
+                day = inputInt("Input the month:");
             }
         }
         if (month == 2) {
             while (day > 29) {
-                print("Максиумальное число дней в феврале - 29!\n");
-                day = inputInt("Введите число месяца:");
+                print("The maximum days in February is 29!\n");
+                day = inputInt("Input the month:");
             }
         }
 
-        year = inputInt("Введите год:");
+        year = inputInt("Input the year:");
         while (year <= 0) {
-            print("Год не может быть отрицательным или равняться нулю!\n");
-            year = inputInt("Введите год:");
+            print("The year must not be negative or equal to 0!\n");
+            year = inputInt("Input the year:");
         }
         if (year < 1582) {
-            print("Введенный год меньше 1582!");
-            print("Точность рассчета високосности не гарантирована!\n");
+            print("The year is less than 1582!");
+            print("The accuracy of leap year calculation is not guaranteed!\n");
         }
         if (!NextDayDaterLogic.isLeapYear(year) && month == 2) {
             while (day > 28) {
-                System.out.printf("%s-ый год - невисокосный, 28 дней в феврале!\n", year);
-                day = inputInt("Введите число месяца:");
+                System.out.printf("%s-th year is non-leap, the February has 28 days!\n", year);
+                day = inputInt("Input the month:");
             }
         }
 
-        print("Дата следующего дня: " + NextDayDaterLogic.tellNextDayDate(day, month, year));
+        print("The date of the next day is " + NextDayDaterLogic.tellNextDayDate(day, month, year));
     }
 }
 
