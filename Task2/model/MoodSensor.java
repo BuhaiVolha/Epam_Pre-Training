3b8c147de48bbca0dfe8a1fe7c0b59a7dd1ab0d4
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-06 11:59:12 +0300

    updated

---
+++ Task2/model/MoodSensor.java
@@ -0,0 +1,31 @@
+package by.epam_pre_training.task2;
+
+import java.util.Random;
+
+public class MoodSensor {
+    public final static int MOOD_NUMBER = 5;
+
+    public static String moodCreator1() {
+        String mood = "";
+
+        switch (new Random().nextInt(MOOD_NUMBER)) {
+            case 0: mood = "=)"; break;
+            case 1: mood = "0_0"; break;
+            case 2: mood = ">:/"; break;
+            case 3: mood = ";D"; break;
+            case 4: mood = ":'["; break;
+        }
+
+        return mood;
+    }
+
+    public static String moodCreator2() {
+        String[] eyes = {"=", ":", ";", "8", ">", ")="};
+        String[] nose = {"", "-"};
+        String[] mouth = {"3", ")", "/", "O", "P", "(", "D", "C"};
+
+        return "" + eyes[new Random().nextInt(eyes.length)] 
+                + nose[new Random().nextInt(nose.length)] 
+                + mouth[new Random().nextInt(mouth.length)];
+    }
+}
