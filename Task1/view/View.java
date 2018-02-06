02abbd71d5e28abbcf3cc35f305fefa356405c39
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-06 15:55:37 +0300

    Update and rename Task1/Printer.java to Task1/view/View.java

--- Task1/view/View.java
+++ Task1/view/View.java
@@ -1,6 +1,8 @@
+package by.epam_pre_training.task1;
+
 import java.util.Scanner;
 
-public class Printer {
+public class View {
 
     public static void print(String msg) {
         System.out.println(msg);
@@ -10,12 +12,12 @@ public class Printer {
         System.out.print(msg);
     }
 
-    public static int inputInt(String msg) {
+    public static int inputInt(String msg){
         print(msg);
         return new Scanner(System.in).nextInt();
     }
 
-    public static String inputLine(String msg) {
+    public static String inputLine(String msg){
         print(msg);
         return new Scanner(System.in).nextLine();
     }
