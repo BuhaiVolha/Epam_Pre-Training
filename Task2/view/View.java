15499d4acb7da140b1172f4a27ddc6df410f391a
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-06 11:42:12 +0300

    Create View.java

---
+++ Task2/view/View.java
@@ -0,0 +1,12 @@
+package by.epam_pre_training.task2;
+
+public class View {
+
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void printThere(String msg) {
+        System.out.print(msg);
+    }
+}
