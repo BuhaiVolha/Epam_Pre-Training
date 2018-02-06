96c3812d86af5cceae45d67187dbaec9bc8a36e0
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-06 15:45:46 +0300

    updated

--- Task2/5/NumbersMaker.java
+++ Task2/5/NumbersMaker.java
@@ -1,74 +1,92 @@
-import static Epam_Pre-Training.Task2.Printer.inputInt;
-import static Epam_Pre-Training.Task2.Printer.print;
+package by.epam_pre_training.task2;
 
 public class NumbersMaker {
 
-    public static void main(String[] args) {
-        int number;
-        boolean isWrong;
+    public static String convertToWord(int n) {
+        final int FIRST_DIGIT = n / 100;
+        final int SECOND_DIGIT = (n / 10) % 10;
+        final int THIRD_DIGIT = n % 10;
 
-        do {
-            number = inputInt("Введите натуральное число от 1 до 999:");
+        String firstDigit = "";
+        String secondDigit = "";
+        String thirdDigit = "";
 
-            if (number <= 0 || number > 999) {
-                print("Число должно находиться в диапазоне от 1 до 999!");
-                isWrong = true;
-            } else {
-                isWrong = false;
-            }
-        } while (isWrong);
+        if (n <= 0 || n > 999) {
+            throw new IllegalArgumentException("The number " +
+                    "must be between 1 and 999!");
+        }
 
-        print(NumbersMakerLogic.convertToWords(number));
+        if (FIRST_DIGIT != 0) {
+            firstDigit = getOnes(FIRST_DIGIT)
+                    + ((FIRST_DIGIT == 1) ? " hundred"
+                    : " hundreds" );
+        }
+        
+        if (SECOND_DIGIT != 0) {
+            secondDigit = (SECOND_DIGIT != 1)
+                    ? getTens(SECOND_DIGIT) + " "
+                    : getAnotherTens(THIRD_DIGIT);
+
+        }
+
+        if (SECOND_DIGIT != 1) {
+            thirdDigit = getOnes(THIRD_DIGIT);
+        }
+        
+        String and = ((FIRST_DIGIT == 0)
+                || (SECOND_DIGIT == 0) && (THIRD_DIGIT == 0))
+                ? "": " and ";
+
+        return firstDigit + and + secondDigit + thirdDigit;
     }
-}
 
-class NumbersMakerLogic {
-
-    public static String convertToWords(int n) {
+    private static String getOnes(int n) {
         String word = "";
-        int hundreds = n / 100;
-        int tenth = n / 10 % 10;
-        int ones = n % 10;
 
-        switch (hundreds) {
-            case 1: word += "сто "; break;
-            case 2: word += "двести "; break;
-            case 3: word += "триста "; break;
-            case 4: word += "четыреста "; break;
-            case 5: word += "пятьсот "; break;
-            case 6: word += "шестьсот "; break;
-            case 7: word += "семьсот "; break;
-            case 8: word += "восемьсот "; break;
-            case 9: word += "девятьсот "; break;
-            default: break;
+        switch (n) {
+            case 1: word += "one"; break;
+            case 2: word += "two"; break;
+            case 3: word += "three"; break;
+            case 4: word += "four"; break;
+            case 5: word += "five"; break;
+            case 6: word += "six"; break;
+            case 7: word += "seven"; break;
+            case 8: word += "eight"; break;
+            case 9: word += "nine"; break;
         }
+        return word;
+    }
 
-        switch (tenth) {
-            case 1: word += "десять "; break;
-            case 2: word += "двадцать "; break;
-            case 3: word += "тридцать "; break;
-            case 4: word += "сорок "; break;
-            case 5: word += "пятьдесят "; break;
-            case 6: word += "шестьдесят "; break;
-            case 7: word += "семьдесят "; break;
-            case 8: word += "восемьдесят "; break;
-            case 9: word += "девяносто "; break;
-            default: break;
+    private static String getTens(int n) {
+        String word = "";
+
+        switch (n) {
+            case 2: word += "twenty"; break;
+            case 3: word += "thirty"; break;
+            case 4: word += "forty"; break;
+            case 5: word += "fifty"; break;
+            case 6: word += "sixty"; break;
+            case 7: word += "seventy"; break;
+            case 8: word += "eighty"; break;
+            case 9: word += "ninety"; break;
         }
+        return word;
+    }
 
-        switch (ones) {
-            case 1: word += "один"; break;
-            case 2: word += "два"; break;
-            case 3: word += "три"; break;
-            case 4: word += "четыре"; break;
-            case 5: word += "пять"; break;
-            case 6: word += "шесть"; break;
-            case 7: word += "семь"; break;
-            case 8: word += "восемь"; break;
-            case 9: word += "девять"; break;
-            default: break;
+    private static String getAnotherTens(int n) {
+        String word = "";
+
+        switch (n) {
+            case 1: word += "eleven"; break;
+            case 2: word += "twelve"; break;
+            case 3: word += "thirteen"; break;
+            case 4: word += "fourteen"; break;
+            case 5: word += "fifteen"; break;
+            case 6: word += "sixteen"; break;
+            case 7: word += "seventeen"; break;
+            case 8: word += "eighteen"; break;
+            case 9: word += "nineteen"; break;
         }
-
         return word;
     }
 }
