e08ad03f87c8e1e585b94c99cf0171edbeead69a
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-08 21:16:15 +0300

    Create NotNaturalNumberException.java

---
+++ Task3/exceptions/NotNaturalNumberException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task3.exceptions;
+
+public class NotNaturalNumberException extends Exception {
+
+    public NotNaturalNumberException(String message) {
+        super(message);
+    }
+}
