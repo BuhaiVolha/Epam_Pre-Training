d6e8629f08936f8f1ef252b00b49a74f6fe7c8da
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-09 20:44:18 +0300

    Update PerfectNumberChecker.java

--- Task3/model/PerfectNumberChecker.java
+++ Task3/model/PerfectNumberChecker.java
@@ -3,6 +3,11 @@ package by.epam_pre_training.task3.model;
 import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
 
 public class PerfectNumberChecker {
+    private static final int FIRST_PERFECT_NUMBER = 6;
+    private static final int SECOND_PERFECT_NUMBER = 28;
+    private static final int THIRD_PERFECT_NUMBER = 496;
+    private static final int FOURTH_PERFECT_NUMBER = 8128;
+    private static final int FIFTH_PERFECT_NUMBER = 33_550_336;
 
     public static boolean isPerfect(int number) throws NotNaturalNumberException {
         int sum = 0;
@@ -27,10 +32,10 @@ public class PerfectNumberChecker {
             throw new NotNaturalNumberException("The number is not a natural number");
         }
 
-        return (number == 6) 
-                || (number == 28)
-                || (number == 496)
-                || (number == 8128)
-                || (number == 33550336);
+        return (number == FIRST_PERFECT_NUMBER) 
+                || (number == SECOND_PERFECT_NUMBER)
+                || (number == THIRD_PERFECT_NUMBER)
+                || (number == FOURTH_PERFECT_NUMBER)
+                || (number == FIFTH_PERFECT_NUMBER);
     }
 }
