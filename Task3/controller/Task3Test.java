fad9915c6a203f83799c1b430184130f0fc9d29e
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-09 19:42:53 +0300

    done

---
+++ Task3/controller/Task3Test.java
@@ -0,0 +1,110 @@
+package by.epam_pre_training.task3.controller;
+
+import static by.epam_pre_training.task3.View.*;
+
+import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
+import by.epam_pre_training.task3.model.*;
+
+public class Task3Test {
+
+    public static void main(String[] args) {
+
+        // Task1
+
+        int heads = HeadsOrTails.throwCoin();
+        int tails = 1000 - heads;
+
+        print("In 1000 throws the coin landed "
+                + heads + " times on heads and "
+                + tails + " times on tails");
+
+        // Task 2.1
+
+        int naturalNumber = 274553981;
+        try {
+            int largestDigit = TheLargestDigitFinder.findLargestDigit(naturalNumber);
+
+            print("The largest digit in a natural number "
+                    + naturalNumber + " is "
+                    + largestDigit);
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 2.2
+
+        int number = 612216;
+
+        try {
+            print("The number " + number + " is "
+                    + ((PalindromeChecker.isPalindrome(number))
+                    ? "a palindrome!" : "not a palindrome!"));
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 2.3
+
+        int possiblyPrimeNumber = 27644437;
+
+        try {
+            print("The number " + possiblyPrimeNumber + " is "
+                    + ((PrimeChecker.isPrime(possiblyPrimeNumber))
+                    ? "a prime!" : "not a prime!"));
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 2.4
+
+        int numberToFactorize = 36473826;
+
+        try {
+            print("The prime dividers of " + numberToFactorize + " are "
+                    + PrimeDividersFinder.findPrimeDividers(numberToFactorize));
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 2.5
+
+        int firstNumber = 25;
+        int secondNumber = 10;
+
+        try {
+            int gcd = GCDAndLCMFinder.getGreatestCommonDivisor(firstNumber, secondNumber);
+            int lcm = GCDAndLCMFinder.getLeastCommonMultiply(firstNumber, secondNumber);
+
+            print("The numbers are " + firstNumber
+                    + " and " + secondNumber
+                    + ",\nthe greatest common divisor is " + gcd
+                    + ",\nthe least common multiply is " + lcm);
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 2.6
+
+        int num = 12665898;
+        try {
+            print("Your number is " + num
+                    + ", the amount of different digits is "
+                    + UniqueDigitsCounter.countUniqueDigits(num));
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+
+        // Task 3
+
+        int maybePerfectNumber = 33550636;
+        try {
+            boolean isPerfect = PerfectNumberChecker.isPerfect(maybePerfectNumber);
+
+            print("The number " + maybePerfectNumber
+                    + " is " + (isPerfect ? "perfect!"
+                    : "not perfect!"));
+        } catch (NotNaturalNumberException e) {
+            print("The number must be larger than 0!");
+        }
+    }
+}
