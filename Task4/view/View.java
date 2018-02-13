dcd35ba49dd797158dcdc4bcc0dc9f0ac91b6eba
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-13 22:21:21 +0300

    Update View.java

--- Task4/view/View.java
+++ Task4/view/View.java
@@ -2,8 +2,8 @@ package by.epam_pre_training.task4.view;
 
 public class View {
 
-    public static void print(String msg) {
-        System.out.println(msg);
+   public static void print(Object object) {
+        System.out.println(object);
     }
 
     public static void printThere(String msg) {
