e9ff620420106e7e195e4158a148cef0e13dfd50
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-14 22:58:43 +0300

    Update LargestDigitFinder.java

--- Task3/model/LargestDigitFinder.java
+++ Task3/model/LargestDigitFinder.java
@@ -1,24 +1,20 @@
 package by.epam_pre_training.task3.model;
 
-import static by.epam_pre_training.task3.validator.Validator.*;
 import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
+import static by.epam_pre_training.task3.validator.Validator.*;
 
-public class LargestDigitFinder {
+public class TheLargestDigitFinder {
 
     public static int findLargestDigit(int naturalNumber) throws NotNaturalNumberException {
-        
+
         validate(naturalNumber);
-       
-        int largestDigit = 0;
-        int digit;
 
-        while (naturalNumber > 0) {
-            digit = naturalNumber % 10;
+        int largestDigit = naturalNumber % 10;
 
-            if (digit > largestDigit) {
-                largestDigit = digit;
-            }
-            naturalNumber /= 10;
+        while ((naturalNumber > 0) && (largestDigit != 9)) {
+            largestDigit = ((naturalNumber /= 10) % 10) > largestDigit
+                            ? naturalNumber % 10
+                            : largestDigit;
         }
         return largestDigit;
     }
