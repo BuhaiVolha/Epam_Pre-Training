a16e2e1c3e50c16bdc6151c95485d47216328cc4
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-14 21:24:37 +0300

    Update PerfectNumberChecker.java

--- Task3/model/PerfectNumberChecker.java
+++ Task3/model/PerfectNumberChecker.java
@@ -1,17 +1,16 @@
 package by.epam_pre_training.task3.model;
 
+import static by.epam_pre_training.task3.validator.Validator.*;
 import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
 
 public class PerfectNumberChecker {
 
     public static boolean checkIfPerfect(int number) throws NotNaturalNumberException {
+        
+        validate(number);
+        
         int sum = 1;
         int top = number / 2;
-
-        if (number <= 0) {
-            throw new NotNaturalNumberException("The number is not a natural number");
-        }
-
 
         for (int i = 2; i <= top; i++) {
             if ((number % i) == 0) {
