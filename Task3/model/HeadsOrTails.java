2b51d15ca27c35890d346f9faf563002ec71c120
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-14 18:20:43 +0300

    Update HeadsOrTails.java

--- Task3/model/HeadsOrTails.java
+++ Task3/model/HeadsOrTails.java
@@ -3,15 +3,13 @@ package by.epam_pre_training.task3.model;
 import java.util.Random;
 
 public class HeadsOrTails {
-    private final static int AMOUNT_OF_THROWS = 1000;
-    private final static int SIDES_OF_COINS = 2;
 
-    public static int throwCoin() {
+    public static int throwCoin(int throwsAmount) {
         int headsCounter = 0;
-        
-        // 1 - for heads
-        for (int i = 0; i < AMOUNT_OF_THROWS; i++) {
-            if (new Random().nextInt(SIDES_OF_COINS) == 1) {
+        Random rand = new Random();
+
+        for (int i = 0; i < throwsAmount; i++) {
+            if (rand.nextBoolean()) {
                 headsCounter++;
             }
         }
