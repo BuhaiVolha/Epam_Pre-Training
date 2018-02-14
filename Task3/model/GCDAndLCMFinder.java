72af2b838c9e8cb6ce6729241043fe2897ca9200
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-14 21:23:56 +0300

    Update GCDAndLCMFinder.java

--- Task3/model/GCDAndLCMFinder.java
+++ Task3/model/GCDAndLCMFinder.java
@@ -1,12 +1,13 @@
 package by.epam_pre_training.task3.model;
 
+import static by.epam_pre_training.task3.validator.Validator.*;
 import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
 
 public class GCDAndLCMFinder {
 
     public static int countGreatestCommonDivisor(int a, int b) throws NotNaturalNumberException {
 
-        Validator.validate(a, b);
+        validate(a, b);
 
         while (a != b) {
             if (a > b) {
@@ -20,9 +21,7 @@ public class GCDAndLCMFinder {
 
     public static int countLeastCommonMultiply(int a, int b) throws NotNaturalNumberException {
 
-        if ((a <= 0) || (b <= 0)) {
-            throw new NotNaturalNumberException("The numbers are not natural numbers");
-        }
+        validate(a, b);
 
         return a / countGreatestCommonDivisor(a, b) * b;
     }
