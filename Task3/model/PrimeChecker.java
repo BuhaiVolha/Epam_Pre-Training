d8657c98a5d5297336550d4cc563ec4aac647411
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-14 21:25:31 +0300

    Update PrimeChecker.java

--- Task3/model/PrimeChecker.java
+++ Task3/model/PrimeChecker.java
@@ -1,17 +1,17 @@
 package by.epam_pre_training.task3.model;
 
+import static by.epam_pre_training.task3.validator.Validator.*;
 import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
 
 public class PrimeChecker {
 
     public static boolean checkIfPrime(int number) throws NotNaturalNumberException {
+        
+        validate(number);
+        
         boolean isPrime = false;
         int squareRoot = (int)Math.sqrt(number);
         
-        if (number <= 0) {
-            throw new NotNaturalNumberException("The number is not a natural number");
-        }
-
         if ((number != 1) && ((number & 1) != 0)) {
             for (int i = 3; i < squareRoot; i += 2) {
                 if ((number % i) == 0) {
