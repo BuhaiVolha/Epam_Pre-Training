bed6f8e316b12d165c2af1b64f739357e117c554
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 13:04:01 +0300

    Create NotPositiveNumberException.java

---
+++ Task4/exceptions/NotPositiveNumberException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task4.exceptions;
+
+public class NotPositiveNumberException extends Exception {
+
+    public NotPositiveNumberException(String message) {
+        super(message);
+    }
+}
