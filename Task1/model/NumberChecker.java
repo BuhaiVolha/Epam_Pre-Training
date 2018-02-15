e7461ee0bf192636863cd31a16be5d5b2b5bed6e
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 23:12:43 +0300

    Update NumberChecker.java

--- Task1/model/NumberChecker.java
+++ Task1/model/NumberChecker.java
@@ -2,7 +2,7 @@ package by.epam_pre_training.task1;
 
 public class NumberChecker {
 
-    public static boolean isRising(int n) {
+    public static boolean checkIfRising(int n) {
         n = Math.abs(n);
         int i = 0;
 
@@ -10,7 +10,7 @@ public class NumberChecker {
         return s.charAt(i++) < s.charAt(i) && s.charAt(i++) < s.charAt(i) && s.charAt(i++) < s.charAt(i);
     }
 
-    public static boolean isDecreasing(int n) {
+    public static boolean checkIfDecreasing(int n) {
         n = Math.abs(n);
         int i = 0;
 
