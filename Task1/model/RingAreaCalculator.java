7d4e53a71d00a5b4dc02d7bbe8121d483f7ea966
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 23:13:34 +0300

    Update RingAreaCalculator.java

--- Task1/model/RingAreaCalculator.java
+++ Task1/model/RingAreaCalculator.java
@@ -2,12 +2,12 @@ package by.epam_pre_training.task1;
 
 public class RingAreaCalculator {
 
-    public static double getRingArea(double r1, double r2) {
-        double s = getCircleArea(r1) - getCircleArea(r2);
+    public static double calculateRingArea(double r1, double r2) {
+        double s = calculateCircleArea(r1) - calculateCircleArea(r2);
         return Math.abs(s);
     }
 
-    private static double getCircleArea(double radius) {
+    private static double calculateCircleArea(double radius) {
         return Math.PI * radius * radius;
     }
 }
