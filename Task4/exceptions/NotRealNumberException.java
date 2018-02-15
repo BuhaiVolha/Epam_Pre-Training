5fe1c8ffddf0b4846b65ea4c90f89ce14e86ab33
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 13:03:36 +0300

    Update and rename NotRealNumberException.java to NumberEqualsZeroException.java

--- Task4/exceptions/NotRealNumberException.java
+++
@@ -1,8 +0,0 @@
-package by.epam_pre_training.task4.exceptions;
-
-public class NotRealNumberException extends Exception {
-
-    public NotRealNumberException(String message) {
-        super(message);
-    }
-}
---
+++ Task4/exceptions/NumberEqualsZeroException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task4.exceptions;
+
+public class NumberEqualsZeroException extends Exception {
+
+    public NumberEqualsZeroException(String message) {
+        super(message);
+    }
+}
