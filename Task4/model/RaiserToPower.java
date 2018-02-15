f3b9da239577f037e7131886509ca52bbbdb45f8
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 11:45:05 +0300

    Update RaiserToPower.java

--- Task4/model/RaiserToPower.java
+++ Task4/model/RaiserToPower.java
@@ -1,10 +1,13 @@
 package by.epam_pre_training.task4.model;
 
-import by.epam_pre_training.task4.exceptions.NotRealNumberException;
+import static by.epam_pre_training.task4.validator.Validator.*;
+import by.epam_pre_training.task4.exceptions.NumberEqualsZeroException;
 
 public class RaiserToPower {
 
-    public static double pow(double a, int b) {
+    public static double pow(double a, int b) throws NumberEqualsZeroException {
+        
+        validateIfZero(a);
 
         if (b > 0) {
             return powRecursive(a, b);
@@ -14,7 +17,7 @@ public class RaiserToPower {
     }
 
     private static double powIterative(double a, int b) {
-        double result = 1;
+        double result = 1.0;
 
         while (b > 0) {
             if ((b & 1) == 1) {
@@ -29,7 +32,7 @@ public class RaiserToPower {
 
 
     private static double powRecursive(double a, int b) {
-        double result
+        double result;
 
         if (b == 0) {
             return 1;
