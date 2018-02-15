7bb3328d11dcaa6abcfbe7d313a7786f5eb819f3
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 23:07:29 +0300

    Update RaiserToPower.java

--- Task4/model/RaiserToPower.java
+++ Task4/model/RaiserToPower.java
@@ -2,17 +2,33 @@ package by.epam_pre_training.task4.model;
 
 public class RaiserToPower {
 
+    // recursive approach
+    
+    public static double powRecursive(double base, int exponent) {
+
+        return (exponent < 0) ? 1.0 / powRecursive(base, -exponent)
+                : (exponent == 0) ? 1
+                : (exponent == 1) ? base
+                : (exponent == 2) ? base * base
+                : ((exponent & 1) == 1) ? powRecursive(base,exponent - 1) * base
+                : powRecursive(base, exponent >> 1)
+                * powRecursive(base, exponent >> 1);
+    }
+    
+    
+    // iterative approach
+  
     public static double pow(double a, int b) {
 
         if (b > 0) {
-            return powRecursive(a, b);
+            return powIterative(a, b);
         } else {
-            return 1 / powRecursive(a, Math.abs(b));
+            return 1 / powIterative(a, -b);
         }
     }
 
     private static double powIterative(double a, int b) {
-        double result = 1;
+        double result = 1.0;
 
         while (b > 0) {
             if ((b & 1) == 1) {
@@ -23,14 +39,5 @@ public class RaiserToPower {
             a *= a;
         }
         return result;
-    }
-
-
-    private static double powRecursive(double a, int b) {
-
-        return (b == 0) ? 1
-                : (b == 2) ? a * a
-                : ((b & 1) == 1) ? powRecursive(a,b - 1) * a
-                : powRecursive(a, b >> 1) * powRecursive(a, b >> 1);
     }
 }
