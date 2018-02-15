64336add30e3b89cb881aec61777f48da131ad32
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 15:49:41 +0300

    Create FibonacciNumbersCounter.java

---
+++ Task4/model/FibonacciNumbersCounter.java
@@ -0,0 +1,31 @@
+package by.epam_pre_training.task4.model;
+
+public class FibonacciNumbersCounter {
+
+    public static int countNthFibonacciNumber(int position) {
+
+        return  (position > 2) ? countNthFibonacciNumber(position - 1)
+                + countNthFibonacciNumber(position - 2)
+                : (position > 0) ? position - 1
+                : -1;
+    }
+
+
+    public static int countNthFibonacciNumberIterative(int position) {
+
+        if (position > 0) {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 2; i <= position; i++) {
+                int temp = a;
+                a += b;
+                b = temp;
+
+            }
+            return a;
+        } else {
+            return -1;
+        }
+    }
+}
