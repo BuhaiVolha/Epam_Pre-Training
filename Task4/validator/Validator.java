80d6e4112e8f08ea4cd7eca93ff9ec377da62cdb
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-15 20:02:32 +0300

    Update Validator.java

--- Task4/validator/Validator.java
+++ Task4/validator/Validator.java
@@ -1,36 +1,36 @@
 package by.epam_pre_training.task4.validator;
 
-import by.epam_pre_training.task3.exceptions.NotNaturalNumberException;
+import by.epam_pre_training.task4.exceptions.NotNaturalNumberException;
 import by.epam_pre_training.task4.exceptions.NotPositiveNumberException;
 import by.epam_pre_training.task4.exceptions.NumberEqualsZeroException;
 
 public class Validator {
 
-    public static void validateIfNatural(int... number) throws NotNaturalNumberException {
+    public static void validateIfNatural(int... numbers) throws NotNaturalNumberException {
 
-        for (int a : number) {
-            if (a <= 0) {
-                throw new NotNaturalNumberException("The number is not a natural number");
+        for (int number : numbers) {
+            if (number <= 0) {
+                throw new NotNaturalNumberException(number + " is not a natural number!");
             }
         }
     }
 
 
-    public static void validateIfPositive(int... number) throws NotPositiveNumberException {
+    public static void validateIfPositive(int... numbers) throws NotPositiveNumberException {
 
-        for (int a : number) {
-            if (a < 0) {
-                throw new NotPositiveNumberException("The number is not a positive number");
+        for (int number : numbers) {
+            if (number < 0) {
+                throw new NotPositiveNumberException(number + " is not a positive number!");
             }
         }
     }
 
 
-    public static void validateIfZero(double... number) throws NumberEqualsZeroException {
+    public static void validateIfZero(double... numbers) throws NumberEqualsZeroException {
 
-        for (double a : number) {
-            if (a == 0) {
-                throw new NumberEqualsZeroException("The number is equal to 0");
+        for (double number : numbers) {
+            if (number == 0) {
+                throw new NumberEqualsZeroException(number + " is equal to 0!");
             }
         }
     }
