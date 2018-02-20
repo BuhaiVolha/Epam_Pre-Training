f97367af591902cf0c7a2cafe1d95e1e437561b5
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 14:14:45 +0300

    Create MagicItem.java

---
+++ Task5/entity/MagicItem.java
@@ -0,0 +1,56 @@
+package by.epam_pre_training.task5.entity;
+
+import java.util.Objects;
+
+public class MagicItem {
+
+    private String name;
+    private int magicPower;
+
+    public MagicItem() {}
+
+    public MagicItem(String name, int magicPower) {
+        this.name = name;
+        this.magicPower = magicPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMagicPower() {
+        return magicPower;
+    }
+
+    public void setMagicPower(int magicPower) {
+        this.magicPower = magicPower;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof MagicItem)) return false;
+        MagicItem magicItem = (MagicItem) o;
+        return getMagicPower() == magicItem.getMagicPower() &&
+                Objects.equals(getName(), magicItem.getName());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getName(), getMagicPower());
+    }
+
+    @Override
+    public String toString() {
+        return "name: "
+                + name
+                + ", magic power: "
+                + magicPower;
+    }
+}
