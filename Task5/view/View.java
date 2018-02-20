09a179f107d1e1c43bd8543991347358b4bd4c11
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 20:09:51 +0300

    Update View.java

--- Task5/view/View.java
+++ Task5/view/View.java
@@ -1,12 +1,23 @@
 package by.epam_pre_training.task5.view;
 
+import by.epam_pre_training.task5.utils.MyList;
+
 public class View {
 
     public static void print(Object object) {
         System.out.println(object);
     }
 
+
     public static void printThere(String msg) {
         System.out.print(msg);
+    }
+
+
+    public static <T> void printElementsOfArray(MyList<T> array) {
+
+        for (T i : array) {
+            print(i);
+        }
     }
 }
