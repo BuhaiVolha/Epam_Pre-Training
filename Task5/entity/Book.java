9dc9788de8d50a1a28b01258da2c35cb2d712cd2
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 20:52:07 +0300

    Update Book.java

--- Task5/entity/Book.java
+++ Task5/entity/Book.java
@@ -23,11 +23,11 @@ public class Book extends MagicItem {
 
     @Override
     public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
         if (this == o) {
             return true;
-        }
-        if (!(o instanceof Book)) {
-            return false;
         }
         if (!super.equals(o)) {
             return false;
