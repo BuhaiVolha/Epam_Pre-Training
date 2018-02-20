d186d6f82295666917cdf511ff691ba3056d4224
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 12:48:56 +0300

    Create LogicTask5Exception.java

---
+++ Task5/exceptions/LogicTask5Exception.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task5.exceptions;
+
+public class LogicTask5Exception extends Task5Exception {
+
+    public LogicTask5Exception(String message) {
+        super(message);
+    }
+}
