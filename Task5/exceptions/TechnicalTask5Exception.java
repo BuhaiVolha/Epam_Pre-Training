eb02af5a9007d94c01cac5edf9a916512f328ed2
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 12:50:01 +0300

    Create TechnicalTask5Exception.java

---
+++ Task5/exceptions/TechnicalTask5Exception.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task5.exceptions;
+
+public class TechnicalTask5Exception extends Task5Exception {
+
+    public TechnicalTask5Exception(String message) {
+        super(message);
+    }
+}
