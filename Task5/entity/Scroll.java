a5523f2cb07dca15a3de300344698f77834b0de8
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 14:14:16 +0300

    Create Scroll.java

---
+++ Task5/entity/Scroll.java
@@ -0,0 +1,53 @@
+package by.epam_pre_training.task5.entity;
+
+import java.util.Objects;
+
+public class Scroll extends MagicItem {
+
+    private int levelOfSpell;
+
+    public Scroll() {}
+
+    public Scroll(String name, int magicPower, int levelOfSpell) {
+        super(name, magicPower);
+        this.levelOfSpell = levelOfSpell;
+    }
+
+    public int getLevelOfSpell() {
+        return levelOfSpell;
+    }
+
+    public void setLevelOfSpell(int levelOfSpell) {
+        this.levelOfSpell = levelOfSpell;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Scroll)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        Scroll scroll = (Scroll) o;
+        return getLevelOfSpell() == scroll.getLevelOfSpell();
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), getLevelOfSpell());
+    }
+
+    @Override
+    public String toString() {
+        return "Scroll, "
+                + super.toString()
+                + ", level of the spell: "
+                + levelOfSpell
+                + ".";
+    }
+}
