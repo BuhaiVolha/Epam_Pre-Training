6262e6af8c7903548020f1309778dac32d7ee3e8
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-20 19:48:05 +0300

    Update Logic.java

--- Task5/logic/Logic.java
+++ Task5/logic/Logic.java
@@ -1,14 +1,14 @@
 package by.epam_pre_training.task5.model;
 
+import static by.epam_pre_training.task5.view.View.*;
 import by.epam_pre_training.task5.entity.MagicItem;
 import by.epam_pre_training.task5.entity.MagicStorage;
 import by.epam_pre_training.task5.utils.MyList;
-import static by.epam_pre_training.task5.view.View.*;
 
 public class Logic {
 
     public static int countTotalMagicPower(MagicStorage storage) {
-        MyList<MagicItem> items = storage.getMagicStorage();
+        MyList<MagicItem> items = getItemsFromStorage(storage);
         int sum = 0;
 
         for (MagicItem item : items) {
@@ -19,7 +19,7 @@ public class Logic {
 
 
     public static MagicItem findMostPowerfulItem(MagicStorage storage) {
-        MyList<MagicItem> items = storage.getMagicStorage();
+        MyList<MagicItem> items = getItemsFromStorage(storage);
         int maxMagicPower = -1;
         MagicItem mostPowerfulItem = null;
 
@@ -32,12 +32,16 @@ public class Logic {
         return mostPowerfulItem;
     }
 
-
     public static void showStorageItems(MagicStorage storage) {
-        MyList<MagicItem> items = storage.getMagicStorage();
+        MyList<MagicItem> items = getItemsFromStorage(storage);
 
         for (MagicItem i : items) {
             print(i);
         }
+    }
+
+
+    private static MyList<MagicItem> getItemsFromStorage(MagicStorage storage) {
+        return storage.getMagicStorage();
     }
 }
