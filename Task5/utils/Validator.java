b87fc2e4365a94aa225f583959f0dbee4848aa56
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-21 16:13:45 +0300

    Update Validator.java

--- Task5/utils/Validator.java
+++ Task5/utils/Validator.java
@@ -1,21 +1,21 @@
 package by.epam_pre_training.task5.utils;
 
 import by.epam_pre_training.task5.exceptions.EmptyArrayException;
-import by.epam_pre_training.task5.exceptions.NotPositiveNumberException;
+import by.epam_pre_training.task5.exceptions.IllegalArrayLengthException;
 
 public class Validator {
 
-    public static void validateIfPositive(int... numbers) throws NotPositiveNumberException {
+    public static void validateArrayLengthArgument(int length) throws IllegalArrayLengthException {
 
-        for (int number : numbers) {
-            if (number < 0) {
-                throw new NotPositiveNumberException(number + " is not a positive number!");
-            }
+        if (length <= 0) {
+            throw new IllegalArrayLengthException("The length of array "
+                    + length
+                    + " is negative or equals to zero!");
         }
     }
 
 
-    public static <T> void checkIfArrayIsEmpty(MyList<T> array) throws EmptyArrayException {
+    public static <T> void validateArrayLength(MyList<T> array) throws EmptyArrayException {
 
         if ((array == null)
                 || (array.size() == 0)) {
