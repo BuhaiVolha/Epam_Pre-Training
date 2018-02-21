72eeee577a234fa5c16686acd4963a9c30b04a19
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-21 15:11:12 +0300

    Create EmptyArrayException.java

---
+++ Task5/exceptions/EmptyArrayException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task5.exceptions;
+
+public class EmptyArrayException extends LogicTask5Exception {
+
+    public EmptyArrayException(String message) {
+        super(message);
+    }
+}
