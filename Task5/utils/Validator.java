675f81cb120797de24d6ab42cb116d823b0bd48f
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-21 15:09:08 +0300

    Update Validator.java

--- Task5/utils/Validator.java
+++ Task5/utils/Validator.java
@@ -1,5 +1,6 @@
-package by.epam_pre_training.task5.validator;
+package by.epam_pre_training.task5.utils;
 
+import by.epam_pre_training.task5.exceptions.EmptyArrayException;
 import by.epam_pre_training.task5.exceptions.NotPositiveNumberException;
 
 public class Validator {
@@ -10,6 +11,14 @@ public class Validator {
             if (number < 0) {
                 throw new NotPositiveNumberException(number + " is not a positive number!");
             }
+        }
+    }
+
+
+    public static <T> void checkIfArrayIsEmpty(MyList<T> array) throws EmptyArrayException {
+
+        if (array.isEmpty()) {
+            throw new EmptyArrayException("The array is empty!");
         }
     }
 }
