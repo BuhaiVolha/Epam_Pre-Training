d7a9bd28bef7e3bc470f1d516568d57e6d89da50
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-21 16:13:20 +0300

    Update and rename NotPositiveNumberException.java to IllegalArrayLengthException.java

---
+++ Task5/exceptions/IllegalArrayLengthException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task5.exceptions;
+
+public class IllegalArrayLengthException extends LogicTask5Exception {
+
+    public IllegalArrayLengthException(String message) {
+        super(message);
+    }
+}
--- Task5/exceptions/NotPositiveNumberException.java
+++
@@ -1,8 +0,0 @@
-package by.epam_pre_training.task5.exceptions;
-
-public class NotPositiveNumberException extends LogicTask5Exception {
-
-    public NotPositiveNumberException(String message) {
-        super(message);
-    }
-}
