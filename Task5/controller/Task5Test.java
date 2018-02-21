f0b9f569ec6ebffd23a90a47ee027135b2ac9745
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-21 15:13:17 +0300

    Update Task5Test.java

--- Task5/controller/Task5Test.java
+++ Task5/controller/Task5Test.java
@@ -1,9 +1,12 @@
 package by.epam_pre_training.task5.controller;
 
 import static by.epam_pre_training.task5.model.Logic.*;
+import by.epam_pre_training.task5.entity.MagicItem;
 import by.epam_pre_training.task5.entity.MagicStorage;
 import by.epam_pre_training.task5.exceptions.Task5Exception;
-import static by.epam_pre_training.task5.validator.Validator.*;
+import static by.epam_pre_training.task5.utils.MagicItemCreator.*;
+import by.epam_pre_training.task5.utils.MyList;
+import static by.epam_pre_training.task5.utils.Validator.*;
 import static by.epam_pre_training.task5.view.View.*;
 
 public class Task5Test {
@@ -12,25 +15,23 @@ public class Task5Test {
 
         try {
             String magicStorageName = "Tower of Magi";
+
             int amountOfItemsInside = 8;
-            
             validateIfPositive(amountOfItemsInside);
-            
-            // Create magicStorage using parameters from above
-            MagicStorage magicStorage = new MagicStorage(magicStorageName, amountOfItemsInside);
-            
-            // Get information about magicStorage using toString()
+
+            MyList<MagicItem> items = createMagicItemArray(amountOfItemsInside);
+            checkIfArrayIsEmpty(items);
+
+            MagicStorage magicStorage = new MagicStorage(magicStorageName, amountOfItemsInside, items);
+
             print(magicStorage);
-            
-            // Print magicStorage's items
-            printElementsOfArray(getItemsFromStorage(magicStorage));
-            
-            // Use Logic's methods
+
+            printElementsOfArray(magicStorage.getItems());
+
             print("\nTotal magic power: " +
                     countTotalMagicPower(magicStorage));
             print("\nThe most powerful item: " +
                     findMostPowerfulItem(magicStorage));
-            
         } catch (Task5Exception e) {
             print(e.getMessage());
         }
