b26e5e5e176fac42bec41e1233bf0e924e363df4
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-22 23:24:53 +0300

    Update FibonacciNumbersCounter.java

--- Task4/model/FibonacciNumbersCounter.java
+++ Task4/model/FibonacciNumbersCounter.java
@@ -4,29 +4,31 @@ public class FibonacciNumbersCounter {
 
     public static int countNthFibonacciNumber(int position) {
 
-        return  (position > 2) ? countNthFibonacciNumber(position - 1)
-                + countNthFibonacciNumber(position - 2)
-                : (position > 0) ? position - 1
-                : -1;
+        if (position > 2) {
+            return countNthFibonacciNumber(position - 1)
+                    + countNthFibonacciNumber(position - 2);
+        }
+        if (position > 0) {
+            return  position - 1;
+        }
+        return -1;
     }
 
 
     public static int countNthFibonacciNumberIterative(int position) {
-        int temp;
-        
+
         if (position > 0) {
             int a = 0;
             int b = 1;
 
             for (int i = 2; i <= position; i++) {
-                temp = a;
+                int temp = a;
                 a += b;
                 b = temp;
 
             }
             return a;
-        } else {
-            return -1;
         }
+        return -1;
     }
 }
