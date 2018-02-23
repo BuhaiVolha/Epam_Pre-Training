4c771902d3eeec3b1b9f323e91c164b0b95ee39c
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-23 11:29:25 +0300

    Create Logic.java

---
+++ task6/model/Logic.java
@@ -0,0 +1,24 @@
+package by.epam.buhai.task6.model;
+
+import by.epam.buhai.task6.exceptions.StackEmptyException;
+import by.epam.buhai.task6.exceptions.StackOverflowException;
+import by.epam.buhai.task6.utils.AbstractMyStack;
+import by.epam.buhai.task6.utils.MyStackArray;
+
+public class Logic {
+
+    public static boolean checkIfPalindrome(String s) throws StackOverflowException, StackEmptyException {
+        AbstractMyStack<Character> string = new MyStackArray<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            string.push(s.charAt(i));
+        }
+
+        String reversedString = "";
+
+        while(!string.isEmpty()) {
+            reversedString += string.pop();
+        }
+        return s.equals(reversedString);
+    }
+}
