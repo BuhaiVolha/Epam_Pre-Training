527c2a5980d784a6696b3d13158c11fc93156afa
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-26 20:15:38 +0300

    Update HanoiTower.java

--- Task4/model/HanoiTower.java
+++ Task4/model/HanoiTower.java
@@ -3,13 +3,24 @@ package by.epam_pre_training.task4.model;
 import static by.epam_pre_training.task4.view.View.*;
 
 public class HanoiTower {
-
-    public static void moveTower(int rings, char source, char receiver, char temp) {
-
-        if (rings != 0) {
-            moveTower(rings - 1, source, temp, receiver);
-            print(source + "-->" + receiver);
-            moveTower(rings - 1, temp, receiver, source);
+    
+    public static String showTowerMovement(int rings, char source, char receiver, char temp) {
+        
+        if (rings == 0) {
+            return "";
         }
+        return moveTower(Math.abs(rings), source, receiver, temp).toString();
+    }
+    
+    
+    private static StringBuilder moveTower(int rings, char source, char receiver, char temp) {
+        StringBuilder stringWithTowerMoves = new StringBuilder();
+
+        if (rings > 0) {
+            stringWithTowerMoves.append(moveTower(rings - 1, source, temp, receiver));
+            stringWithTowerMoves.append(source + " --> " + receiver + "\n");
+            stringWithTowerMoves.append(moveTower(rings - 1, temp, receiver, source));
+        }
+        return stringWithTowerMoves;
     }
 }
