91e362145da9a061c154e6296790f89d48c806b0
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-02-27 20:14:46 +0300

    Create MyStackLinkedList.java

---
+++ task6/model/stack/MyStackLinkedList.java
@@ -0,0 +1,61 @@
+package by.epam_pre_training.task6.model.stack;
+
+import by.epam_pre_training.task6.exceptions.StackEmptyException;
+
+public class MyStackLinkedList<E> implements AbstractMyStack {
+
+    private Node head;
+    private int size;
+
+    private class Node {
+        E element;
+        Node next;
+    }
+
+    public MyStackLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public E pop() {
+        E item = head.element;
+        head = head.next;
+        size--;
+        return item;
+    }
+
+    @Override
+    public void push(Object element) {
+        Node oldHead = head;
+        head = new Node();
+        head.element = (E) element;
+        head.next = oldHead;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Override
+    public boolean isFull() {
+        return size() == size;
+    }
+
+
+    @Override
+    public E peek() throws StackEmptyException {
+        if (isEmpty()) {
+            throw new StackEmptyException("The stack is empty!");
+        }
+        return head.element;
+    }
+}
