630a34b9a5fc87bd618d524dccababf0d18aab4a
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:35:52 +0300

    Create LogicTask6Exception.java

---
+++ task6/exceptions/LogicTask6Exception.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task6.exceptions;
+
+public class LogicTask6Exception extends Task6Exception {
+
+    public LogicTask6Exception(String message) {
+        super(message);
+    }
+}
