afb9db116000770321e4453d0c74b78e6a42250d
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:37:39 +0300

    Update Logic.java

--- task6/model/Logic.java
+++ task6/model/Logic.java
@@ -1,23 +1,29 @@
 package by.epam_pre_training.task6.model;
 
-import by.epam_pre_training.task6.model.stack.AbstractMyStack;
-import by.epam_pre_training.task6.model.stack.MyStackFixedSizeArray;
+import by.epam_pre_training.task6.exceptions.Task6Exception;
+import by.epam_pre_training.task6.model.containers.stack.AbstractStack;
+import by.epam_pre_training.task6.model.containers.stack.StackFixedSizeArrayBased;
 
 public class Logic {
 
-    public static boolean checkIfPalindrome(String s) {
-        s = s.toLowerCase();
-        AbstractMyStack<Character> string = new MyStackFixedSizeArray<>(s.length());
+    public static boolean checkIfPalindrome(String word) throws Task6Exception {
+        word = word.toLowerCase();
+        boolean isPalindrome = true;
 
-        for (int i = 0; i < s.length(); i++) {
-            string.push(s.charAt(i));
+        // pushing in first half of the word
+        int wordLength = word.length() / 2;
+        AbstractStack<Character> stack = new StackFixedSizeArrayBased<>(wordLength);
+        for (int i = 0; i < wordLength; i++) {
+            stack.push(word.charAt(i));
         }
 
-       String reversedString = "";
-
-        while(!string.isEmpty()) {
-            reversedString += string.pop();
+        // checking starting from the middle
+        int j = ((word.length() & 1) == 0) ? wordLength : wordLength + 1;
+        while (!stack.isEmpty() && isPalindrome) {
+            if (stack.pop() != word.charAt(j++)) {
+                isPalindrome = false;
+            }
         }
-        return s.equals(reversedString);
+        return isPalindrome;
     }
 }
