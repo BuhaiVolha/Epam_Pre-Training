6b8622fa978846fd0cb87bddd28c3fd76b402763
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:49:35 +0300

    Update and rename task6/model/stack/MyStackDynamicArray.java to task6/model/containers/stack/StackDynamicArrayBased.java

---
+++ task6/model/containers/stack/StackDynamicArrayBased.java
@@ -0,0 +1,32 @@
+package by.epam_pre_training.task6.model.containers.stack;
+
+public class StackDynamicArrayBased<E> extends ArrayBase<E> implements AbstractStack<E> {
+
+    @SuppressWarnings("unchecked")
+    public StackDynamicArrayBased(int size) {
+        array = (E[]) new Object[size];
+        top = 0;
+    }
+
+    public StackDynamicArrayBased() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @Override
+    public void push(E element) {
+        if (size() == array.length) {
+            expand();
+        }
+        array[top] = element;
+        top++;
+    }
+
+
+    @SuppressWarnings("unchecked")
+    private void expand() {
+        E[] newArray = (E[]) new Object[array.length * 2];
+
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+}
