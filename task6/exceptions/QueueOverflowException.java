6e67f5d73563ea1931bf90b4edd28591835ae0a0
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:36:24 +0300

    Create QueueOverflowException.java

---
+++ task6/exceptions/QueueOverflowException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task6.exceptions;
+
+public class QueueOverflowException extends LogicTask6Exception {
+
+    public QueueOverflowException(String msg) {
+        super(msg);
+    }
+}
