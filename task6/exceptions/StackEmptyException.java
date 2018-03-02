c682fe0cd87c3ec8fa2b3d81e1d0bd53fc192b16
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:34:52 +0300

    Update StackEmptyException.java

--- task6/exceptions/StackEmptyException.java
+++ task6/exceptions/StackEmptyException.java
@@ -1,6 +1,6 @@
 package by.epam_pre_training.task6.exceptions;
 
-public class StackEmptyException extends RuntimeException {
+public class StackEmptyException extends LogicTask6Exception {
 
     public StackEmptyException(String msg) {
         super(msg);
