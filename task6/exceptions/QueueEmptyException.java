7792d6a8d99d2c7c21df3cc198b5d746c4d5039e
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-02 04:36:10 +0300

    Create QueueEmptyException.java

---
+++ task6/exceptions/QueueEmptyException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task6.exceptions;
+
+public class QueueEmptyException extends LogicTask6Exception {
+
+    public QueueEmptyException(String msg) {
+        super(msg);
+    }
+}
