2f2fed775326d9e4f60eaba94e6e1c07fe1111c4
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-05 02:07:38 +0300

    Create LogicTask7Exception.java

---
+++ task7/exceptions/LogicTask7Exception.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task7.exceptions;
+
+public class LogicTask7Exception extends Task7Exception {
+
+    public LogicTask7Exception(String message) {
+        super(message);
+    }
+}
