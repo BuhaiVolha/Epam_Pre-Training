8f0b1182bba1600f83a6e9058f67126c54889613
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-05 02:09:00 +0300

    Create Tree.java

---
+++ task7/logic/Tree.java
@@ -0,0 +1,15 @@
+package by.epam_pre_training.task7.logic;
+
+import by.epam_pre_training.task7.exceptions.EmptyTreeException;
+
+public interface Tree<E> {
+
+    void add(E data);
+    boolean contains(E data) throws EmptyTreeException;
+    void remove(E data) throws EmptyTreeException;
+    void clear();
+    int size();
+    String traversePreOrder();
+    String traversePostOrder();
+    String traverseInOrder();
+}
