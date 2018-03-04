ade52d8df42d72b11f0c69c896aa178523c49e0a
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-05 02:07:55 +0300

    Create EmptyTreeException.java

---
+++ task7/exceptions/EmptyTreeException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task7.exceptions;
+
+public class EmptyTreeException extends LogicTask7Exception {
+
+    public EmptyTreeException(String message) {
+        super(message);
+    }
+}
