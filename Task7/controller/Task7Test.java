3e2160d466fa8ad958a634ad8559f51d4ed135fb
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-06 01:30:56 +0300

    Update Task7Test.java

--- Task7/controller/Task7Test.java
+++ Task7/controller/Task7Test.java
@@ -1,50 +1,59 @@
 package by.epam_pre_training.task7.controller;
 
 import by.epam_pre_training.task7.entity.Cat;
-import by.epam_pre_training.task7.model.AbstractTree;
-import by.epam_pre_training.task7.model.BinaryTree;
-import static by.epam_pre_training.task7.view.View.*;
+import by.epam_pre_training.task7.logic.AbstractTree;
+import by.epam_pre_training.task7.logic.BinaryTree;
+import static  by.epam_pre_training.task7.view.View.*;
 import java.util.Comparator;
 
 public class Task7Test {
 
     public static void main(String[] args) {
 
-        Comparator<Cat> byName = Cat.CatNameComparator;
-        Comparator<Cat> byAge = Cat.CatAgeComparator;
+        // creating a binary tree of Cats based on mouse caught number
+
         Comparator<Cat> byMouses = Cat.CatMouseCaughtNumberComparator;
 
         AbstractTree<Cat> cats = new BinaryTree<>(byMouses);
+        
         cats.add(new Cat("Barsik", 6, 15));
         cats.add(new Cat("Lapka", 8, 4));
         cats.add(new Cat("Murzik", 3, 22));
         cats.add(new Cat("Kruzis", 1, 7));
 
+        print("There are " 
+                + cats.size() 
+                + " cats on the tree:\n" 
+                + cats);
+
         try {
-            cats.remove(new Cat("Murzik", 3, 22));
-            print(cats.contains(new Cat("Murzik", 3, 22)));
+            Cat toBeDeletedCat = new Cat("Murzik", 3, 22);
+
+            print("Getting "
+                    + toBeDeletedCat.getName()
+                    + " out of the tree...");
+            cats.remove(toBeDeletedCat);
+
+            print(toBeDeletedCat.getName()
+                    + " is on the tree: "
+                    + cats.contains(toBeDeletedCat));
+
         } catch (Exception e) {
             print(e.getMessage());
         }
 
+        print("\nPrint cats in order:");
         print(cats.traverseInOrder());
 
+        print("Print cats in post-order:");
+        print(cats.traversePostOrder());
 
-        AbstractTree<Integer> integers = new BinaryTree<>();
-        integers.add(19);
-        integers.add(3);
-        integers.add(13);
-        integers.add(33);
-        integers.add(73);
+        print("Print cats in pre-order:");
+        print(cats.traversePreOrder());
 
-        try {
-            integers.remove(19);
-            print(integers.contains(19));
-        } catch (Exception e) {
-            print(e.getMessage());
-        }
+        print("Clearing the tree...");
+        cats.clear();
+        print("Cats on the tree: " + cats.size());
 
-        print(integers.traverseInOrder());
-        integers.clear();
     }
 }
