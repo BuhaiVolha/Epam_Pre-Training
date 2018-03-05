0aee94256ba1917573ef15be7c700ec205149b02
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-06 01:12:45 +0300

    Create LogicTask8Exception.java

---
+++ Task8/exceptions/LogicTask8Exception.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task8.exceptions;
+
+public class LogicTask8Exception extends Task8Exception {
+
+    public LogicTask8Exception(String message) {
+        super(message);
+    }
+}
