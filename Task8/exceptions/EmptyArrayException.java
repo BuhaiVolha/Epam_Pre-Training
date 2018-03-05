eb4f160e23fac0a2f65b2a2b54fed96e0b414548
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-06 01:13:05 +0300

    Create EmptyArrayException.java

---
+++ Task8/exceptions/EmptyArrayException.java
@@ -0,0 +1,8 @@
+package by.epam_pre_training.task8.exceptions;
+
+public class EmptyArrayException extends LogicTask8Exception {
+
+    public EmptyArrayException(String message) {
+        super(message);
+    }
+}
