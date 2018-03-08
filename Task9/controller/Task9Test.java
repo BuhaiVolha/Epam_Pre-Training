ef3fa6f0372fd3f4f1ba84b6c4f3a0d189d156fe
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-08 22:22:51 +0300

    Create Task9Test.java

---
+++ Task9/controller/Task9Test.java
@@ -0,0 +1,55 @@
+package by.epam_pre_training.task9.controller;
+
+import by.epam_pre_training.task9.entity.Text;
+import by.epam_pre_training.task9.exceptions.Task9Exception;
+import by.epam_pre_training.task9.model.Logic;
+import by.epam_pre_training.task9.model.TextWorker;
+import java.io.File;
+
+import static by.epam_pre_training.task9.view.View.*;
+
+public class Task9Test {
+
+    public static void main(String[] args) {
+
+        // setting path
+        String separator = File.separator;
+        String path = "D:" + separator + "document.txt";
+
+        // reading a file
+        try {
+            TextWorker textWorker = new TextWorker(path);
+            textWorker.readText();
+
+            print("Source text:\n");
+            print(textWorker.getText());
+
+            print("Parsing the text and rebuilding it:\n");
+            Text text = textWorker.parseText();
+            print(textWorker.rebuildText(text));
+
+
+            // Task 14
+            // The longest palindrome substring
+
+            Text task14 = textWorker.parseText();
+            String[] result = Logic.findLongestPalindromeSubstring(task14).split(" ");
+            print("The word with the longest palindrome substring is "
+                    + result[0]
+                    + ", the palindrome is "
+                    + result[1]
+                    + ".");
+
+
+            // Task 15
+
+            Text task15 = textWorker.parseText();
+            print("\nChanging every word by deleting all occurrences of it's first letter in it:\n");
+            print(Logic.removeFirstLetterEveryWhereInTheWord(task15));
+
+        } catch (Task9Exception e) {
+            print(e.getCause());
+            print(e.getMessage());
+        }
+    }
+}
