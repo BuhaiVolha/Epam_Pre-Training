c7452bb1c8bef36b847d553b9d22e7d1f66cea04
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-08 22:14:35 +0300

    Create Task9Exception.java

---
+++ Task9/exceptions/Task9Exception.java
@@ -0,0 +1,16 @@
+package by.epam_pre_training.task9.exceptions;
+
+public class Task9Exception extends Exception {
+
+    public Task9Exception(String msg) {
+        super(msg);
+    }
+
+    public Task9Exception(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public Task9Exception(Throwable cause) {
+        super(cause);
+    }
+}
