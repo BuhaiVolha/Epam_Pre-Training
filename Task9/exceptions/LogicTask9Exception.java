541d36562a6dd8c476f7b3e62e82d1e108c4a2c3
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-03-08 22:14:54 +0300

    Create LogicTask9Exception.java

---
+++ Task9/exceptions/LogicTask9Exception.java
@@ -0,0 +1,16 @@
+package by.epam_pre_training.task9.exceptions;
+
+public class LogicTask9Exception extends Task9Exception {
+
+    public LogicTask9Exception(String msg) {
+        super(msg);
+    }
+
+    public LogicTask9Exception(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public LogicTask9Exception(Throwable cause) {
+        super(cause);
+    }
+}
